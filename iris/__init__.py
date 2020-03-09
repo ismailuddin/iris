ee3612acfc6c216b20ebf72f0541d87945aa6bce
Ismail Uddin <ismail.sameeuddin@gmail.com>
2020-03-09 15:56:25 -0700

    Place .iris folder inside data folder and application context

--- iris/__init__.py
+++ iris/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from .config.flask import Config
+from .models.db import db
 
-app = Flask(__name__, static_folder="../public", static_url_path="/public")
-app.config.from_object(Config)
-db = SQLAlchemy(app)
 
-from .routes.general import general
-from .routes.api import api
+def create_app(instance_path: str, database_uri: str):
+    app = Flask(__name__, static_folder="../public", static_url_path="/public")
+    app.config.from_object(Config)
+    app.config["INSTANCE_PATH"] = instance_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
 
-app.register_blueprint(general)
-app.register_blueprint(api)
+    db.init_app(app)
+
+    from .routes.general import general
+    from .routes.api import api
+
+    app.register_blueprint(general)
+    app.register_blueprint(api)
+    return app
--- iris/__main__.py
+++ iris/__main__.py
@@ -2,7 +2,8 @@ import os
 import pathlib
 import glob
 import click
-from iris import app, db
+from iris import create_app
+from iris.models.db import db
 from iris.models.files import File
 from iris.utils.files import build_filelist, populate_db_with_filelist
 
@@ -15,22 +16,27 @@ def main():
 @main.command(help="Launch server")
 @click.option("--folder", "-f", help="Folder to discover files")
 @click.option("--extension", "-e", help="Extension to filter on", default=".png")
-def launch(folder: str, extension: str):
+@click.option("--port", "-p", help="Port to run server on", default=5000)
+def launch(folder: str, extension: str, port: int):
     database_dir = pathlib.Path(
-        os.path.join(os.getcwd(), ".iris")
+        os.path.join(os.getcwd(), folder, ".iris")
     )
     database_dir.mkdir(exist_ok=True, parents=True)
     database_file = pathlib.Path(os.path.join(database_dir, "data.db"))
     glob_pattern = os.path.join(folder, "**", f"*{extension}")
-    if not database_file.exists():
-        db.create_all()
-        filelist = build_filelist(glob_pattern)
-        print(f"Found {len(filelist)} files.")
-        print("Populating database...")
-        populate_db_with_filelist(filelist)
+    app = create_app(
+        instance_path=os.path.join(folder),
+        database_uri=f"sqlite:///{database_file}"
+    )
+    with app.app_context():
+        if not database_file.exists():
+            db.create_all()
+            filelist = build_filelist(glob_pattern)
+            print(f"Found {len(filelist)} files.")
+            print("Populating database...")
+            populate_db_with_filelist(filelist)
 
-    app.config["INSTANCE_PATH"] = os.path.join(folder)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 if __name__ == "__main__":
     launch()
